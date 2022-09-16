76dfa3070237b5a42af68e46eca8cc8e08c2221f
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-09-15 23:13:45 -0300

    created Modal and form - class 03

--- src/components/CreateAdBanner.tsx
+++ src/components/CreateAdBanner.tsx
@@ -1,3 +1,4 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import { MagnifyingGlassPlus } from 'phosphor-react';
 
 export function CreateAdBanner() {
@@ -9,10 +10,10 @@ export function CreateAdBanner() {
           <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
         </div>
 
-        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+        <Dialog.Trigger className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
           <MagnifyingGlassPlus size={24} />
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
       </div>
     </div>
   )
